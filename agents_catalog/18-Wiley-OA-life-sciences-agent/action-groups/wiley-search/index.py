124924da0665d1c24b181f310817e4bd520e5f38
Brian Loyal <bloyal@amazon.com>
2025-06-30 12:49:05 -0500

    Wiley agent api fix (#139)
    * manually provide missing value from function event format
    
    * Update wiley agent to use function response formatting

--- agents_catalog/18-Wiley-OA-life-sciences-agent/action-groups/wiley-search/index.py
+++ agents_catalog/18-Wiley-OA-life-sciences-agent/action-groups/wiley-search/index.py
@@ -25,7 +25,7 @@ def lambda_handler(event, context):
         logger.info(f"Received query parameter 'question': {question}")
     else:
         response_code = 400
-        response_body = {"text/plain": {"body": "Query parameter 'question' is missing"}}
+        response_body = {"TEXT": {"body": "Query parameter 'question' is missing"}}
         logger.warning("Query parameter 'question' is missing")
 
 
@@ -38,30 +38,39 @@ def lambda_handler(event, context):
                 external_data = response.read().decode(charset)
                 json_data = json.loads(external_data)
                 logger.info(f"Received external data: {json_data}")
-                response_body = {"application/json": {"body": json_data}}
+                response_body = {"TEXT": {"body": str(json_data)}}
 
         except urllib.error.URLError as e:
-            response_body = {"text/plain": {"body": f"Request failed: {e.reason}"}}
+            response_body = {"TEXT": {"body": f"Request failed: {e.reason}"}}
             response_code = 500
 
         except json.JSONDecodeError:
-            response_body = {"text/plain": {"body": "Failed to parse JSON response"}}
+            response_body = {"TEXT": {"body": "Failed to parse JSON response"}}
             response_code = 500
 
     action_response = {
         "actionGroup": event["actionGroup"],
-        "apiPath": event["apiPath"],
-        "httpMethod": event["httpMethod"],
-        "httpStatusCode": response_code,
-        "responseBody": response_body,
+        "function": event['function'],
+        "functionResponse": {
+            "responseBody": response_body,
+        }
     }
+
+    logger.info(f"Action response is\n{action_response}")
 
     session_attributes = event.get("sessionAttributes", {})
     prompt_session_attributes = event.get("promptSessionAttributes", {})
+    
+    
 
-    return {
+    response =  {
         "messageVersion": "1.0",
         "response": action_response,
         "sessionAttributes": session_attributes,
         "promptSessionAttributes": prompt_session_attributes,
     }
+
+    logger.info(f"Function response is\n{response}")
+
+
+    return response
